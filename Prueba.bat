efb12503231aee1c02e1a2e6d6cb3096684d91ff
Camilo <54856835+cjgomezr@users.noreply.github.com>
2024-05-12 16:25:10 +1000

    Update Prueba.bat

--- Prueba.bat
+++ Prueba.bat
@@ -1,1 +1,1 @@
-"C:\Users\gnpedidos\AppData\Local\UiPath\app-19.10.0-beta0484\UiRobot.exe" -file "C:\Users\gnpedidos\Downloads\EnvioMensajeLogs.xaml"
+"C:\Users\User\AppData\Local\UiPath\app-19.10.0-beta0484\UiRobot.exe" -file "C:\Users\User\Downloads\EnvioMensajeLogs.xaml"
